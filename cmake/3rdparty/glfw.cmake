ad20a3012e42bf689ed66f0a4d0f03fccabd101b
Andrew Naumov <softmob@mail.ru>
2023-11-27 19:34:11 +0100

    Fix test run

---
+++ cmake/3rdparty/glfw.cmake
@@ -0,0 +1,13 @@
+if (BUILD_SAMPLES)
+    return()
+endif()
+
+option(GLFW_BUILD_EXAMPLES "Build the GLFW example programs" OFF)
+option(GLFW_BUILD_TESTS "Build the GLFW test programs" OFF)
+option(GLFW_BUILD_DOCS "Build the GLFW documentation" OFF)
+option(GLFW_INSTALL "Generate installation target" OFF)
+
+add_subdirectory(${project_root}/3rdparty/FlyWrapper/3rdparty/FlyCube/3rdparty/glfw glfw EXCLUDE_FROM_ALL)
+set_target_properties(glfw PROPERTIES FOLDER "3rdparty/glfw")
+target_include_directories(glfw INTERFACE "${project_root}/3rdparty/glfw/include")
+target_compile_definitions(glfw INTERFACE GLFW_INCLUDE_NONE)
