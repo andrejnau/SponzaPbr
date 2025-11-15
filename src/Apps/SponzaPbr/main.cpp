40141da41bb3827ef3edc7b06fce2036156d7066
Andrew Naumov <andrejnau@gmail.com>
2025-11-15 19:37:04 +0100

    Update FlyWrapper

--- src/Apps/SponzaPbr/main.cpp
+++ src/Apps/SponzaPbr/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
     Settings settings = ParseArgs(argc, argv);
     AppBox app("SponzaPbr", settings);
     std::shared_ptr<RenderDevice> device =
-        CreateRenderDevice(settings, app.GetNativeWindow(), app.GetAppSize().width(), app.GetAppSize().height());
+        CreateRenderDevice(settings, app.GetNativeSurface(), app.GetAppSize().width(), app.GetAppSize().height());
     Scene scene(app, settings, std::move(device));
     while (!app.PollEvents()) {
         scene.RenderFrame();
